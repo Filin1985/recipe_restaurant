a1ecf7876ad18ea83645f8ac1f842c743057daa8
Marat Ikhsanov <ihsmen@gmail.com>
2023-06-21 09:15:08 +0300

    feat: Реализовать поиск рецептов и их отображение.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,11 +1,15 @@
 import * as model from './model'
 import recipeView from './views/recipeView'
+import searchView from './views/searchView'
+import resultsView from './views/resultsView'
 
 import 'core-js/stable'
 
 // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept()
+}
 
 const controlRecipes = async function () {
   try {
@@ -17,10 +21,26 @@ const controlRecipes = async function () {
 
     recipeView.render(model.state.recipe)
   } catch (error) {
+    recipeView.renderError()
+  }
+}
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner()
+    const query = searchView.getQuery()
+    if (!query) return
+
+    await model.loadSearchResults(query)
+    resultsView.render(model.state.search.results)
+  } catch (error) {
     console.log(error)
   }
 }
 
-;['hashchange', 'load'].forEach((ev) =>
-  window.addEventListener(ev, controlRecipes)
-)
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes)
+  searchView.addHandlerSearch(controlSearchResults)
+}
+
+init()
--- src/js/model.js
+++ src/js/model.js
@@ -3,6 +3,10 @@ import {getJSON} from './helpers'
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: []
+  },
 }
 
 export const loadRecipe = async (id) => {
@@ -23,3 +27,24 @@ export const loadRecipe = async (id) => {
     throw error
   }
 }
+
+export const loadSearchResults = async function(query) {
+  try {
+    state.search.query = query
+
+    const data = await getJSON(`${API_URL}/?search=${query}`)
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      }
+    })
+  } catch (error) {
+    console.error(error);
+    throw error
+  }
+}
+
+loadSearchResults('pizza')
